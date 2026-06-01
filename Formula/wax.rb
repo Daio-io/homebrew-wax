7e43112b2c62567c2344a057fee8d567ab016530
daio-io <dle.williams1@gmail.com>
2026-06-01 19:18:47 +0100

    feat: add wax formula

---
+++ Formula/wax.rb
@@ -0,0 +1,35 @@
+class Wax < Formula
+  desc "Design-system component tracker CLI"
+  homepage "https://github.com/Daio-io/wax"
+  version "0.1.0-alpha.1"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/Daio-io/wax/releases/download/v#{version}/wax-#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "d1a56d34e3fcf2401e8b8075e20f51785b3a2aa6e868c2f9146a7659fe6b0a79"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/Daio-io/wax/releases/download/v#{version}/wax-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "7577f80c04e83fa6ac74045c36160aa2f29b5f1877a1e61978788ea3ed447ede"
+    end
+  end
+
+  def install
+    bin.install "wax-#{version}-#{Hardware::CPU.arm? ? "aarch64-apple-darwin" : "x86_64-apple-darwin"}/wax" => "wax"
+  end
+
+  def caveats
+    <<~EOS
+      Language packs are not bundled with the CLI binary.
+
+      After installing wax, run:
+        wax init --non-interactive --language compose
+        wax language install compose
+    EOS
+  end
+
+  test do
+    assert_match "wax", shell_output("#{bin}/wax --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew Tap for wax
+
+```bash
+brew tap Daio-io/wax
+brew install wax
+```
+
+The formula installs the `wax` CLI binary. Language packs are downloaded on demand by `wax`.
