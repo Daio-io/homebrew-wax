a1d431947f6d425301351840e4b3942f5f305613
daio-io <dle.williams1@gmail.com>
2026-06-12 08:01:37 +0100

    fix: bump wax formula to v0.1.0-alpha.6

--- Formula/wax.rb
+++ Formula/wax.rb
@@ -5,12 +5,12 @@ class Wax < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Daio-io/wax/releases/download/v0.1.0-alpha.3/wax-0.1.0-alpha.3-aarch64-apple-darwin.tar.gz"
-      sha256 "1074f5399f57e663772562e8552180a27dbc3e30c720d3d96f3e49b73400abd2"
+      url "https://github.com/Daio-io/wax/releases/download/v0.1.0-alpha.6/wax-0.1.0-alpha.6-aarch64-apple-darwin.tar.gz"
+      sha256 "fd3ec88cea8070f323a2ef858e8f01d9fa4db3cfe1a71055c659c0794689d961"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Daio-io/wax/releases/download/v0.1.0-alpha.3/wax-0.1.0-alpha.3-x86_64-apple-darwin.tar.gz"
-      sha256 "e170b59c313d301fac7f3604d813863dd1d801977618e5a769bfb675fab95c46"
+      url "https://github.com/Daio-io/wax/releases/download/v0.1.0-alpha.6/wax-0.1.0-alpha.6-x86_64-apple-darwin.tar.gz"
+      sha256 "4124707a8caad013f727d7936f07ae8d912c9520d98d44575f44437d7d61f9c7"
     end
   end
 
